4cdd6857b2d6c761c52d60663916413b57b89993
Jady Lima <jadylsilva@gmail.com>
2024-05-02 16:50:07 -0300

    Update 03_Validando_a_Forca_de_Senhas_no_IAM.py

--- Intermediario/03_Validando_a_Forca_de_Senhas_no_IAM.py
+++ Intermediario/03_Validando_a_Forca_de_Senhas_no_IAM.py
@@ -7,7 +7,7 @@ def verificar_forca_senha(senha):
   tem_letra_minuscula = False
   tem_numero = False
   tem_caractere_especial = False
-
+  
   # Verificando o comprimento da senha
   if len(senha) < comprimento_minimo:
     return f"Sua senha e muito curta. Recomenda-se no minimo {comprimento_minimo} caracteres."
@@ -19,14 +19,17 @@ def verificar_forca_senha(senha):
   # Verificando se a senha contém letras minúsculas
   if (re.search(r'[a-z]', senha)):
     tem_letra_minuscula =  True
+    
+  # Verificando se a senha contém caracteres especiais
+  caracteres_especiais = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '.', '<', '>', '?', '`', '~', '\\']
+  for especial in caracteres_especiais:
+    if especial in senha:
+      tem_caractere_especial = True
+      break
 
   # Verificando se a senha contém números
   if (re.search(r'[0-9]', senha)):
     tem_numero = True
-  
-  # Verificando se a senha contém caracteres especiais
-  if (re.search(r'[@*/!#$%+-_¨&()`^~:;.,|{}=?[\]´]', senha)):
-    tem_caractere_especial = True
 
   # Verificando se a senha contém sequências comuns
   sequencias_comuns = ["123456", "abcdef"]
@@ -39,15 +42,11 @@ def verificar_forca_senha(senha):
   if senha in palavras_comuns:
     return "Sua senha e muito comum. Tente uma senha mais complexa."
 
-  for i in range(len(senha) - 2):
-    if ord(senha[i]) == (ord(senha[i+1]) - 1) == (ord(senha[i+2]) - 2):
-      return "Sua senha nao atende aos requisitos de seguranca."
-
   # TODO: Verificar o comprimento mínimo e critérios de validação
-  if (tem_letra_maiuscula == True and tem_letra_minuscula == True and tem_numero == True and tem_caractere_especial == True):
-    return "Sua senha atende aos requisitos de seguranca. Parabens!"
-  else: 
+  if (tem_letra_maiuscula != True or tem_letra_minuscula != True or tem_numero != True or tem_caractere_especial != True):
     return "Sua senha nao atende aos requisitos de seguranca."
+  else: 
+    return "Sua senha atende aos requisitos de seguranca. Parabens!"
 
 # Obtendo a senha do usuário
 senha = input().strip()
