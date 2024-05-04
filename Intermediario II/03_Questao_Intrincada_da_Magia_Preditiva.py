90692fa528930f90c8600c200193bede974220e1
Jady Lima <jadylsilva@gmail.com>
2024-05-04 09:46:33 -0300

    Update 03_Questao_Intrincada_da_Magia_Preditiva.py

--- Intermediario II/03_Questao_Intrincada_da_Magia_Preditiva.py
+++ Intermediario II/03_Questao_Intrincada_da_Magia_Preditiva.py
@@ -4,10 +4,20 @@ def prever_afinidade_elemental(intensidade, componente_raro, fase_lunar, idade_f
     componente_raro = componente_raro.lower() == "sim"
     afinidade_animais = afinidade_animais.lower() == "sim"
 
+    print(f"`{intensidade}, {componente_raro}, {fase_lunar}, {idade_feiticeiro}, {afinidade_animais}")
+
     # Desenvolva a Lógica de decisão para prever a afinidade elemental
     if intensidade >= 5 and fase_lunar == "crescente" and idade_feiticeiro > 100:
         return "A afinidade elemental do feiticeiro é com o elemento Fogo!"
-   
+        
+    elif intensidade >= 7 and componente_raro == True and fase_lunar == "cheia" and idade_feiticeiro <= 100 and afinidade_animais == False:
+        return "A afinidade elemental do feiticeiro é com o elemento Água!"
+    
+    elif intensidade >= 7 and componente_raro == True and fase_lunar == "cheia" and idade_feiticeiro <= 100 and afinidade_animais == True:
+        return "A afinidade elemental do feiticeiro é com o elemento Terra!"
+    
+    else:
+        return "A afinidade elemental do feiticeiro é com o elemento Ar!"
 
 # Entrada do usuário
 intensidade_feitico = int(input())
