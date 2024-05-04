257db856a4ff92f369961d8c69b743dcda9a0371
Jady Lima <jadylsilva@gmail.com>
2024-05-04 09:46:24 -0300

    Update 01_Robo_Inteligente.py

--- Intermediario II/01_Robo_Inteligente.py
+++ Intermediario II/01_Robo_Inteligente.py
@@ -1,6 +1,10 @@
-# TODO: Crie a Função para classificar um número como positivo, negativo ou zero
 def classificar_numero(numero):
-    return
+    if numero < 0:
+        return "negativo!"
+    elif numero > 0:
+        return "positivo!"
+    else:
+        return
     
 def main():
     positivos = 0
@@ -10,15 +14,15 @@ def main():
         numero = int(input())
         
         if numero == 0:
-            break  # Encerra o loop se o usuário digitar 0.
-        
-        # Chama a função classificar_numero para imprimir a classificação do número
+            break 
+
         print(classificar_numero(numero))
         
-        # TODO: Faça a verificação para saber quantos números positivos e negativos foram inseridos
+        if classificar_numero(numero) == "positivo!":
+            positivos += 1
+        elif classificar_numero(numero) == "negativo!":
+            negativos += 1
      
-    
-    # Imprime a quantidade de números positivos e negativos inseridos
     print(f"{positivos} números positivos, {negativos} números negativos")
 
 if __name__ == "__main__":
